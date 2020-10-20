201f9bd4beb962249dd2c1e4c6322b00612c1fd3
竹帅 <5318773+ch_zs@user.noreply.gitee.com>
2020-10-20 09:30:29 +0800

    添加服装类,服装分类

---
+++ models/index.py
@@ -0,0 +1,28 @@
+from . import db
+
+
+class Product(db.Model):  # 货号表
+    """服装"""
+    __tablename__ = "product"
+
+    id = db.Column(db.Integer, primary_key=True)  # 货品编号
+    title = db.Column(db.String(256), nullable=False)  # 品牌名
+    color = db.Column(db.String(256), nullable=False)  # 服装颜色
+    size = db.Column(db.String(256), nullable=False)  # 服装尺寸
+    content = db.Column(db.Text, nullable=False)  # 服装类型
+    factory_price = db.Column(db.String(64), nullable=False)  # 出厂价
+    selling_price = db.Column(db.String(64), nullable=False)  # 零售价
+    digest = db.Column(db.String(512), nullable=False)  # 产品信息
+    clicks = db.Column(db.Integer, default=0)  # 出厂日期
+
+
+
+class Category(db.Model):
+    """服装分类"""
+    __tablename__ = "category"
+
+    id = db.Column(db.Integer, primary_key=True)  # 分类编号
+    grop_name = db.Column(db.String(64), nullable=False)  # 分类名
+    type_name = db.Column(db.String(64), nullable=False)  # 类型名
+
+
