ee5997fb4373ae86c51a01ed9b68fb3a2502e15b
竹帅 <5318773+ch_zs@user.noreply.gitee.com>
2020-10-20 20:47:09 +0800

    跟新模型类,添加男女分类,服装类型分类

--- models/index.py
+++ models/index.py
@@ -15,9 +15,7 @@ class Product(db.Model):  # 货号表
     digest = db.Column(db.String(512), nullable=False)  # 产品信息
     img_url = db.Column(db.String(256), nullable=False)  # 产品图片/显示在首页的
 
-
     clicks = db.Column(db.Integer, default=0)  # 出厂日期
-
 
 
 class Category(db.Model):
@@ -25,7 +23,7 @@ class Category(db.Model):
     __tablename__ = "category"
 
     id = db.Column(db.Integer, primary_key=True)  # 分类编号
+    g_id = db.Column(db.Integer, default=False)  # 男女分类
     grop_name = db.Column(db.String(64), nullable=False)  # 分类名
     type_name = db.Column(db.String(64), nullable=False)  # 类型名
-
-
+    t_id = db.Column(db.Integer, default=False)  # 服装类型分类
