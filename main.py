a15319e6dc19e9d232145b7f9a8f11084c8f8534
竹帅 <5318773+ch_zs@user.noreply.gitee.com>
2020-10-20 09:43:27 +0800

    使用manager管理app

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 
 from views import index_blu
 from models import db
+
 # 创建flask应用对象
 app = Flask(__name__)
 
@@ -11,6 +14,13 @@ app.register_blueprint(index_blu)
 # 加载配置
 app.config.from_pyfile("config.ini")
 
+# 添加数据库迁移等工具
+manager = Manager(app)
+# 生成migrate对象 用来数据库迁移
+migrate = Migrate(app, db)
+# 添加db命令
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    app.run(port=8899)
+    # app.run(port=8899)
+    manager.run()
