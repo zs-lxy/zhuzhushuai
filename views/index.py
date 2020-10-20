7d71b405d1debc2631173ded0c3f65fa9f288907
竹帅 <5318773+ch_zs@user.noreply.gitee.com>
2020-10-20 11:49:54 +0800

    显示男款最新12个服装

--- views/index.py
+++ views/index.py
@@ -1,9 +1,13 @@
 from flask import render_template
 
+from models import db
+from models.index import Product
 from . import index_blu
 
 
 @index_blu.route("/")
 def index():
+    # 查询最新的服装
+    product = db.session.query(Product).order_by(-Product.clicks).limit(12)
+    return render_template('index/index.html',product=product)
 
-    return render_template('index/index.html')
