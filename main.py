bfb1b6bd24ff4e29038df9edd11ffc95221e7eba
竹帅 <5318773+ch_zs@user.noreply.gitee.com>
2020-10-20 09:41:40 +0800

    加载配置信息

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from flask import Flask
 
 from views import index_blu
-
+from models import db
 # 创建flask应用对象
 app = Flask(__name__)
 
 # 创建蓝图，且注册到app
 app.register_blueprint(index_blu)
 
-
+# 加载配置
+app.config.from_pyfile("config.ini")
 
 
 if __name__ == '__main__':
