ba0bef0186f93eda782d27f54969871bcab15279
竹帅 <5318773+ch_zs@user.noreply.gitee.com>
2020-10-21 09:13:41 +0800

    显示正确数据

--- views/index.py
+++ views/index.py
@@ -1,13 +1,16 @@
 from flask import render_template
 
 from models import db
-from models.index import Product
+from models.index import Product, Category
 from . import index_blu
 
 
 @index_blu.route("/")
 def index():
-    # 查询最新的服装
-    product = db.session.query(Product).order_by(-Product.clicks).limit(12)
-    return render_template('index/index.html',product=product)
+    # 查询12个男生服装
+    man_dress = db.session.query(Product).filter(Product.one_category_id == 100).order_by(-Product.dress_date).limit(12)
+    # 查询12个女生服装
+    woman_dress = db.session.query(Product).filter(Product.one_category_id == 200).order_by(-Product.dress_date).limit(
+        12)
 
+    return render_template('index/index.html', man_dress=man_dress, woman_dress=woman_dress)
