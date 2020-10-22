81eb2793f1a962102ca0d3f2bce0ed17a9b06d7a
竹帅 <5318773+ch_zs@user.noreply.gitee.com>
2020-10-22 16:46:13 +0800

    点击登录,提交数据,后台收到

--- views/passport.py
+++ views/passport.py
@@ -1,3 +1,23 @@
+from flask import jsonify, request
+
 from . import passport_blu
 
+
 # 这里存放用passport_blu蓝图装饰的视图函数
+
+
+@passport_blu.route("/passport/login_btn", methods=["GET", "POST"])
+def login_btn():
+    print(request.args)
+    print(request.form)
+    username = request.json.get('username')
+    passwd = request.json.get('passwd')
+    print("________++++++++++++___________")
+    print(username, passwd)
+
+    ret = {
+        'errno': 0,
+        'errmsg': "登录成功.....!"
+
+    }
+    return jsonify(ret)
