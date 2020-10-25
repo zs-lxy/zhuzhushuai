da8b6f812ed4046c3a2851eaec9db8ea2bd1940e
竹帅 <5318773+ch_zs@user.noreply.gitee.com>
2020-10-25 12:24:02 +0800

    个人中心的,每个视图的路由都添加完毕(缺少对应的html模板)

--- views/user.py
+++ views/user.py
@@ -14,21 +14,67 @@ def homepage():
     # 查询user
     user = db.session.query(User).filter(User.mobile == user_mobile).first()
     # print(user.mobile)
-    # 如果没有登录...
+    # 如果没有登录...(跳转到首页)
     if not user_mobile:
-        return render_template('index/homepage.html')
+        return render_template('index/index.html')
     else:
         return render_template('index/homepage.html', user=user)
 
 
 @user_blu.route("/user/user_base_info")
 def user_base_info():
-    # 显示个人信息(个人主页的  主页)
-    return render_template("index/user_base_info.html")
+    # 个人主页信息展示
+    user_mobile = session.get('mobile')
+    # 查询user
+    user = db.session.query(User).filter(User.mobile == user_mobile).first()
 
+    # 显示个人信息(个人主页的  主页)
+    return render_template("index/user_base_info.html", user=user)
 
 
 @user_blu.route("/user/my_order")
 def my_order():
-    # 显示个人信息(个人主页的  主页)
+    # 我的订单
     return render_template("index/my_order.html")
+
+
+@user_blu.route("/user/my_evalute")
+def my_evalute():
+    # 我的评价
+    return render_template("index/my_evalute.html")
+
+
+@user_blu.route("/user/my_coupon")
+def my_coupon():
+    # 我的优惠卷
+    return render_template("index/my_coupon.html")
+
+
+@user_blu.route("/user/alter_user_info")
+def alter_user_info():
+    # 修改个人信息
+    return render_template("index/alter_user_info.html")
+
+
+@user_blu.route("/user/alter_password")
+def alter_password():
+    # 修改密码
+    return render_template("index/alter_password.html")
+
+
+@user_blu.route("/user/addrs")
+def addrs():
+    # 收货地址
+    return render_template("index/addrs.html")
+
+
+@user_blu.route("/user/leave_maessage")
+def leave_maessage():
+    # 我的留言
+    return render_template("index/leave_maessage.html")
+
+
+@user_blu.route("/user/my_collect")
+def my_collect():
+    # 我的收藏
+    return render_template("index/my_collect.html")
